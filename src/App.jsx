e4b2720521a60a65a3b051b4dd8e4ff3f6177211
sarveshchand12 <sarvesh.chand12@gmail.com>
2025-07-14 21:18:52 +1200

    Added Incident Reporting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+
+// Import your pages
 import Home from './pages/Home';
+import PhishingEmail from './pages/PhishingEmail';
 import PhishingDemo from './pages/phdemo';
-import PhishingEmail from './pages/PhishingEmail'; 
 import SQLInjection from './pages/sqlinj';
 
-export default function App() {
+function Root() {
   return (
     <Router>
-      <Routes>
-        <Route path="/sql-injection/" element={<Home />} />
-         <Route path="/sql-injection/email" element={<PhishingEmail />} />   {/* ← New route */}
-        <Route path="/sql-injection/phishing" element={<PhishingDemo />} />
-        <Route path="/sql-injection/sqlinjection" element={<SQLInjection />} />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 }
+
+function AppRoutes() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = sessionStorage.redirect;
+    if (path && path !== location.pathname) {
+      sessionStorage.removeItem('redirect');
+      navigate(path);
+    }
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/sql-injection" element={<Home />} />
+      <Route path="/sql-injection/email" element={<PhishingEmail />} />
+      <Route path="/sql-injection/phishing" element={<PhishingDemo />} />
+      <Route path="/sql-injection/sqlinjection" element={<SQLInjection />} />
+    </Routes>
+  );
+}
+
+export default Root;
